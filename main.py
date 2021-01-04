f37d48cf6c8bb5c3f5e70c3e48fae3e0b17f1ead
Joshua <finnthefinder2008@gmail.com>
2021-01-04 18:43:41 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+lista = input("")
+if lista == "import list":
+    listb = input("")
+    listc = input("")
+    listd = input("")
+    liste = input("")
+    listf = input("")
+    listg = input("")
+q = input("")
+if q == "state list":
+    print ("", listb)
+    print ("", listc)
+    print ("", listd)
+    print ("", liste)
+    print ("", listf)
+    print ("", listg)
+    
